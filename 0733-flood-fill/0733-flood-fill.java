3d5da2dc94a479144b3e9316bc9525828a08fb11
manojgowda <136791062+1DS22CS054-BAMANOJGOWDA@users.noreply.github.com>
2026-05-25 19:47:44 +0530

    Time: 0 ms (100%), Space: 46.7 MB (59.82%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int original = image[sr][sc];
+
+        if(original == color){
+            return image;
+        }
+
+        dfs(image,sr,sc,original,color);
+
+        return image;
+    }
+
+    private void dfs(int[][] image,int row,int col,int original,int color){
+        int rows = image.length;
+        int cols = image[0].length;
+
+        if(row < 0 || col < 0 || row >= rows || col >= cols || image[row][col] != original){
+            return;
+        }
+
+        image[row][col] = color;
+
+        dfs(image,row+1,col,original,color);
+        dfs(image,row-1,col,original,color);
+        dfs(image,row,col+1,original,color);
+        dfs(image,row,col-1,original,color);
+    }
+}
