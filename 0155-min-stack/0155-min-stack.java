4a1b49ab3a963ecb0d113c3cbd774c9d3d48c281
manojgowda <136791062+1DS22CS054-BAMANOJGOWDA@users.noreply.github.com>
2026-09-02 00:47:37 +0530

    Time: 29 ms (94.48%), Space: 100.9 MB (35.64%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,34 +1,62 @@
+// class MinStack {
+
+//     Deque<Integer> stack = new ArrayDeque<>();
+//     Deque<Integer> minstack = new ArrayDeque<>();
+//     public MinStack() {
+        
+//     }
+    
+//     public void push(int value) {
+//         stack.push(value);
+//         if(minstack.isEmpty()){
+//             minstack.push(value);
+//         }else{
+//             minstack.push(Math.min(value,minstack.peek()));
+//         }
+//     }
+    
+//     public void pop() {
+//         stack.pop();
+//         minstack.pop();
+//     }
+    
+//     public int top() {
+//         return stack.peek();
+//     }
+    
+//     public int getMin() {
+//         return minstack.peek();
+//     }
+// }
 class MinStack {
 
-    Deque<Integer> stack = new ArrayDeque<>();
-    Deque<Integer> minstack = new ArrayDeque<>();
+    private Deque<int[]> stack = new ArrayDeque<>();
+
     public MinStack() {
-        
     }
-    
-    public void push(int value) {
-        stack.push(value);
-        if(minstack.isEmpty()){
-            minstack.push(value);
-        }else{
-            minstack.push(Math.min(value,minstack.peek()));
+
+    public void push(int val) {
+
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int currentMin = Math.min(val, stack.peek()[1]);
+            stack.push(new int[]{val, currentMin});
         }
     }
-    
+
     public void pop() {
         stack.pop();
-        minstack.pop();
     }
-    
+
     public int top() {
-        return stack.peek();
+        return stack.peek()[0];
     }
-    
+
     public int getMin() {
-        return minstack.peek();
+        return stack.peek()[1];
     }
 }
-
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
